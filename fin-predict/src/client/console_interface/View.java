30d8bb1b9574d25a7cd545954555faae2a0ef053
Tahmid Sajin <tahmid.saj@gmail.com>
2023-05-27 23:50:44 -0400

    Logic for view class

--- fin-predict/src/client/console_interface/View.java
+++ fin-predict/src/client/console_interface/View.java
@@ -1,4 +1,11 @@
 package client.console_interface;
 
+// Console interface:
+// Prompts users for receiving forecasting predictions
+// Prompts users for performing Refresh and Read operations on database
+
+// View class:
+// Abstract class which implements the view interface
+// Contains abstract methods for the Input and Dashboard classes
 public interface View {
 }
