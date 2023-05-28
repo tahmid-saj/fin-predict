8573d512d9b7518ef22be260c6d64f02bf9812a7
Tahmid Sajin <tahmid.saj@gmail.com>
2023-05-28 03:02:46 -0400

    Adding dashboard class

--- fin-predict/src/client/dashboard/Dashboard.java
+++ fin-predict/src/client/dashboard/Dashboard.java
@@ -1,2 +1,15 @@
-package client.dashboard;public class Dashboard {
+package client.dashboard;
+import client.console_interface.ViewDashboard;
+
+public class Dashboard implements ViewDashboard {
+
+    @Override
+    public void setup() {
+
+    }
+
+    @Override
+    public void displayDashboard() {
+
+    }
 }
