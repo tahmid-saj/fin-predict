c3ac9abc28285358b3c808e409bd92b22ccf9e10
tahmid-saj <tahmid.saj@gmail.com>
2024-06-01 13:00:12 -0400

    mvn dependencies init

---
+++ src/main/java/com/ts/finpredict/FinPredict/FinPredictApplication.java
@@ -0,0 +1,13 @@
+package com.ts.finpredict.FinPredict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinPredictApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinPredictApplication.class, args);
+	}
+
+}
